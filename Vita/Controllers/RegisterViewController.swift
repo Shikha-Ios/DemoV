1777642c5864fe44dfac2b6c34052f17b6e6216a
Shemona <shemona.mnd@gmail.com>
2017-07-19 17:49:56 +0530

    Login and Registration Layout Implementation

--- Vita/Controllers/RegisterViewController.swift
+++ Vita/Controllers/RegisterViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,18 +26,87 @@ class RegisterViewController: UIViewController {
     }
     
     //MARK: Action Methods
-    
-    
-    @IBAction func buttonBackClicked(sender: UIButton){
+    @IBAction func backClicked(sender: UIButton){
         self.navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func buttonSignUpClicked(sender: UIButton){
-        self.performSegue(withIdentifier:"CameraDashboard", sender: nil)
+    @IBAction func signUpClicked(sender: UIButton){
         
+        self.performSegue(withIdentifier:"ContainerVC", sender: nil)
+        
+        /*
+        if (!self.checkValidation())
+        {
+            return
+        }
+        else
+        {
+        self.performSegue(withIdentifier:"ContainerVC", sender: nil)
+        }
+ */
+    }
+    
+    @IBAction func facebookSignUpClicked(sender: UIButton){
+    }
+    
+    @IBAction func googlePlusSignUpClicked(sender: UIButton){
+    }
+    
+    @IBAction func privacyClicked(sender: UIButton){
     }
 
+    // MARK: UITextField Delegates
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: -  Input Validation Methods/Alert Methods...
+    func checkValidation() ->Bool
+    {
+        if(userNameTextField.text?.characters.count == 0 && emailAddressTextField.text?.characters.count == 0 && passwordTextField.text?.characters.count == 0 )
+        {
+            print("Please enter information")
+            return false
+        }
+        if (userNameTextField.text?.characters.count == 0)
+        {
+            print("Please enter user name")
+            return false
+        }
+        if (emailAddressTextField.text?.characters.count == 0)
+        {
+            print("Please enter the valid email")
+            return false
+        }
+        if (emailAddressTextField.text?.characters.count == 0)
+        {
+            print("Please enter the valid password")
+            return false
+        }
+        if ((userNameTextField.text?.characters.count)! > 0 && (!self.isValidEmail(email: userNameTextField.text as String!)))
+        {
+            print("Please enter the valid email")
+            return false
+        }
+        return true
+    }
 
+    func isValidPassword(password: String) -> Bool {
+        //Minimum 6 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character:
+        let passwordRegEx = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{6,}"
+        let passwordTest = NSPredicate(format:"SELF MATCHES %@", passwordRegEx)
+        return passwordTest.evaluate(with: password)
+    }
+    
+    func isValidEmail(email:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+    
+       
     /*
     // MARK: - Navigation
 
